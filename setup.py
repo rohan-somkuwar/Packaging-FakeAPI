0efc986bdb0c39305924caa3cd4b30d8e24e836a
RohanS <rohansomkuwar97@gmail.com>
2022-11-19 15:14:43 +0530

    create fakepistories python package

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
-from setuptools import setup
-with open("requirements.txt", "r", encoding="utf-8") as fh:
+from setuptools import setup, find_packages
+with open("requirements.txt", "r") as fh:
     requirements = fh.read()
-setup(name = "fakedata",
-version = 0.2,
-description = "This code is fetching data through the fakedata api",
+setup(name = "fakeapistories",
+version = 0.1,
 author = "Rohan",
 author_email = "rohansomkuwar97@gmail.com",
-packages = ['fake'],
+description = "This code is fetching data through the fakedata api",
+
+url = "https://github.com/rohan-somkuwar/clitool/tree/master",
+py_modules = ['fakeapistories'],
+packages = find_packages(),
 install_requires = [requirements],
 python_requires = ">=3.7",
+classifiers = [
+    "Programming Language :: Python :: 3.8",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+],
+entry_points = """
+[console_scripts]
 
+"""
 )
